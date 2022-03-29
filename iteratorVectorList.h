d59bd1156607396b791d9b6261d269ec088c17c2
Denis Guzhev <guzhev.98@mail.ru>
2022-03-29 15:52:32 +0300

    new

--- iteratorVectorList.h
+++ iteratorVectorList.h
@@ -1,8 +1,8 @@
 #include <vector>
 #include <list>
-#include <iterator>
 
 template<class T>
+
 class VectorList
 {
 private:
@@ -19,14 +19,10 @@ public:
     VectorList & operator=(VectorList &&)     = default;
     VectorList & operator=(VectorList const &) = default;
 
-    void append(std::vector<T>& vect)
-    {
-        data_.push_back(vect);
-    }
     // метод, который будет использоваться для заполнения VectorList
     // гарантирует, что в списке не будет пустых массивов
-  // template<class It>
- //   void append(It p, It q); определена снаружи
+    template<class It>
+    void append(It p, It q); // определена снаружи
 /*  {
         if (p != q)
             data_.push_back(VectT(p,q));
@@ -36,70 +32,74 @@ public:
     bool empty() const { return size() == 0; } 
 
     // определите метод size
- 
+    size_t size() const 
+    {
+        return 0;
+    }
 
-
-    class const_iterator: std::iterator<std::bidirectional_iterator_tag,
+    // определите const_iterator 
+    //создаем класс внутри класса. Класс итератор будем наследовать от стандартного класса итераторов в std;
+    //элементарным элементом на который будет указывать класс будет являться элемент вектор T.
+    class const_iterator :std::iterator<std::bidirectional_iterator_tag,
                                                      T, 
                                                      size_t,
                                                      T*, 
-                                                     T&>
-    {
-
-     
-        public:
-            //Constructor
-            const_iterator()
-            {
-
-            }
-
-
-            const_iterator begin() const { return 0;}
-            const_iterator end() const {return 0; }
-            
-            private:
-           // ListT::iterator lit;
-            typename VectT::iterator vit;
-            typename ListT::iterator lit;
-            ListT* data_;
-
-    };
-
-       //метод size должен вычислять количество всех элементов типа T во ВСЕХ векторах!!
-    size_t size() const 
-    {
-        size_t counter = 0;
-
-        /*for (;it != this.end(); ++it)
-        {++counter;}
-    */
-        return counter;
-    }
-   // определите const_iterator
-
-     /*class const_iterator::std::iterator<std::bidirectional_iterator_tag>
-    {
+                                                     T&> 
+    {   
+        
+        
+        friend class VectorList;
         public:
         const_iterator() = default;
-    };
-    */
-/*
-    // определите методы begin / end
-    const_iterator begin() const { return ... ; }
-    const_iterator end()   const { return ... ; }
+        const_iterator(const const_iterator& other): p(other.p)
+        {}
+        bool operator!=(const_iterator const& other) const
+        {
+            return p != other.p;
+        }
+        bool operator==(const_iterator const& other) const
+        {   
+            return p == other.p;
+        } //need for BOOST_FOREACH
 
-    // определите const_reverse_iterator
-    ... const_reverse_iterator ...
+        T operator*() const
+        {
+            return *p;
+        }
+        const_iterator& operator++()
+        {
+            ++p;
+            return *this;
+        }
+        private:
+        const_iterator(T*p):p(p)
+        {
+           
+        }
+        const_iterator(ListT* data):p(p)
+        {
+           
+        }
 
-    // определите методы rbegin / rend
-    const_reverse_iterator rbegin() const { return ... ;   }
-    const_reverse_iterator rend()   const { return ... ; }
-*/  void show_list_of_vectors ()
-    {
+        T* p;
+        std::vector<T>* vect;
+        std::list<std::vector<T>>* listvect;
+        
 
     
-    }
-private:
+    };
+
+    // определите методы begin / end
+    const_iterator begin() const { return const_iterator(&(this->data_)); }
+   // const_iterator end()   const { return ... ; }
+
+    // определите const_reverse_iterator
+   // class const_reverse_iterator ...
+
+    // определите методы rbegin / rend
+   // const_reverse_iterator rbegin() const { return ... ;   }
+   // const_reverse_iterator rend()   const { return ... ; }*/
+
+    private:
     ListT data_;
 };
